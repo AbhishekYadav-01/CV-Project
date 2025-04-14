725e47b07cb6ba61b3ea601168a09835614b0f22
Abhishek Yadav <145996514+AbhishekYadav-01@users.noreply.github.com>
2025-04-14 15:34:37 +0530

    Create Scene_image_and_Object.py

---
+++ Modules/Scene_image_and_Object.py
@@ -0,0 +1,14 @@
+!pip install --quiet gdown
+import gdown
+
+# URL for the .glb file (3D object)
+url_glb = 'https://drive.google.com/uc?id=1tGyOA98PIxVTP7scK4RfyfpSyzsU0Q-5'
+output_glb = 'object.glb'
+
+# URL for the .jpeg file (scene image)
+url_jpeg = 'https://drive.google.com/uc?id=1G7ZxnwVP7aem7Tv-wGJaI_dASYTxfE6H'
+output_jpeg = 'scene_image.jpeg'
+
+# Download the files
+gdown.download(url_glb, output_glb, quiet=False)
+gdown.download(url_jpeg, output_jpeg, quiet=False)
